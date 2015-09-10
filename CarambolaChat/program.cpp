fa2edee3366daa543050b0db502486553096c571
Magnus Norddahl <dpjudas@users.noreply.github.com>
2015-09-10 14:38:28 +0200

    Fixed OS X build

--- CarambolaChat/program.cpp
+++ CarambolaChat/program.cpp
@@ -2,6 +2,10 @@
 #include "precomp.h"
 #include "program.h"
 
+#ifdef __APPLE__
+#include <CoreFoundation/CoreFoundation.h>
+#endif
+
 using namespace clan;
 
 clan::ApplicationInstance<Program> clanapp;
@@ -9,6 +13,13 @@ clan::ApplicationInstance<Program> clanapp;
 class DisplayResources : public DisplayCache
 {
 public:
+#ifdef __APPLE__
+	DisplayResources()
+	{
+		resource_path() = Directory::get_resourcedata("CarambolaChat", resource_path());
+	}
+#endif
+	
 	Resource<Sprite> get_sprite(Canvas &canvas, const std::string &id) override { throw Exception("No sprite resources"); }
 	Resource<Texture> get_texture(GraphicContext &gc, const std::string &id) override { throw Exception("No texture resources"); }
 
@@ -16,7 +27,7 @@ public:
 	{
 		if (loaded_images.find(id) == loaded_images.end())
 		{
-			loaded_images[id] = Image(canvas, PathHelp::combine(resource_path, id));
+			loaded_images[id] = Image(canvas, PathHelp::combine(resource_path(), id));
 		}
 		return loaded_images[id];
 	}
@@ -30,20 +41,20 @@ public:
 				FontFamily family(family_name);
 
 				FontDescription normal;
-				family.add(normal, PathHelp::combine(resource_path, "SourceSansPro/SourceSansPro-Regular.ttf"));
+				family.add(normal, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-Regular.ttf"));
 
 				FontDescription bold;
 				bold.set_weight(FontWeight::bold);
-				family.add(bold, PathHelp::combine(resource_path, "SourceSansPro/SourceSansPro-Bold.ttf"));
+				family.add(bold, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-Bold.ttf"));
 
 				FontDescription italic;
 				italic.set_style(FontStyle::italic);
-				family.add(italic, PathHelp::combine(resource_path, "SourceSansPro/SourceSansPro-Italic.ttf"));
+				family.add(italic, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-Italic.ttf"));
 
 				FontDescription bold_italic;
 				bold_italic.set_weight(FontWeight::bold);
 				bold_italic.set_style(FontStyle::italic);
-				family.add(bold_italic, PathHelp::combine(resource_path, "SourceSansPro/SourceSansPro-BoldItalic.ttf"));
+				family.add(bold_italic, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-BoldItalic.ttf"));
 
 				loaded_fonts[family_name] = family;
 			}
@@ -55,9 +66,13 @@ public:
 		return Font(loaded_fonts[family_name], desc);
 	}
 
+	static std::string &resource_path()
+	{
+		static std::string resource_path = "Resources";
+		return resource_path;
+	}
 
 private:
-	std::string resource_path = "Resources";
 	std::map<std::string, FontFamily > loaded_fonts;
 	std::map<std::string, Resource<Image>> loaded_images;
 };
@@ -90,12 +105,12 @@ Program::Program()
 	DisplayWindowDescription desc;
 	desc.set_title("Carambola Chat");
 	desc.set_allow_resize(true);
-	desc.set_size(Size(1536, 864), false);
+	desc.set_size(clan::Size(1536, 864), false);
 
 	window = std::make_shared<TopLevelWindow>(desc);
 	window->set_root_view(chat_window_controller->view);
-	window->get_display_window().set_large_icon(ImageProviderFactory::load("Resources/Icons/carambola-256.png"));
-	window->get_display_window().set_small_icon(ImageProviderFactory::load("Resources/Icons/carambola-32.png"));
+	window->get_display_window().set_large_icon(ImageProviderFactory::load(PathHelp::combine(DisplayResources::resource_path(), "Icons/carambola-256.png")));
+	window->get_display_window().set_small_icon(ImageProviderFactory::load(PathHelp::combine(DisplayResources::resource_path(), "Icons/carambola-32.png")));
 	window->show(WindowShowType::show_default);
 
 	Application::use_timeout_timing(250);
