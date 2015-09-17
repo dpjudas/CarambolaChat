4c7657a1b36f805804bda2fcf632c94c19c92de0
Magnus Norddahl <dpjudas@users.noreply.github.com>
2015-09-17 06:43:43 +0200

    Updated resource handling

--- CarambolaChat/program.cpp
+++ CarambolaChat/program.cpp
@@ -2,106 +2,24 @@
 #include "precomp.h"
 #include "program.h"
 
-#ifdef __APPLE__
-#include <CoreFoundation/CoreFoundation.h>
-#endif
-
 using namespace uicore;
 
 uicore::ApplicationInstance<Program> app;
-/*
-class DisplayResources : public DisplayCache
-{
-public:
-#ifdef __APPLE__
-	DisplayResources()
-	{
-		resource_path() = Directory::get_resourcedata("CarambolaChat", resource_path());
-	}
-#endif
-	
-	Resource<Sprite> get_sprite(Canvas &canvas, const std::string &id) override { throw Exception("No sprite resources"); }
-	Resource<Texture> get_texture(GraphicContext &gc, const std::string &id) override { throw Exception("No texture resources"); }
-
-	Resource<Image> get_image(Canvas &canvas, const std::string &id) override
-	{
-		if (loaded_images.find(id) == loaded_images.end())
-		{
-			loaded_images[id] = Image(canvas, PathHelp::combine(resource_path(), id));
-		}
-		return loaded_images[id];
-	}
-
-	Resource<Font> get_font(Canvas &canvas, const std::string &family_name, const FontDescription &desc) override
-	{
-		if (loaded_fonts.find(family_name) == loaded_fonts.end())
-		{
-			if (family_name == "Source Sans Pro")
-			{
-				FontFamily family(family_name);
-
-				FontDescription normal;
-				family.add(normal, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-Regular.ttf"));
-
-				FontDescription bold;
-				bold.set_weight(FontWeight::bold);
-				family.add(bold, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-Bold.ttf"));
-
-				FontDescription italic;
-				italic.set_style(FontStyle::italic);
-				family.add(italic, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-Italic.ttf"));
-
-				FontDescription bold_italic;
-				bold_italic.set_weight(FontWeight::bold);
-				bold_italic.set_style(FontStyle::italic);
-				family.add(bold_italic, PathHelp::combine(resource_path(), "SourceSansPro/SourceSansPro-BoldItalic.ttf"));
-
-				loaded_fonts[family_name] = family;
-			}
-			else
-			{
-				loaded_fonts[family_name] = FontFamily(family_name);
-			}
-		}
-		return Font(loaded_fonts[family_name], desc);
-	}
-
-	static std::string &resource_path()
-	{
-		static std::string resource_path = "Resources";
-		return resource_path;
-	}
-
-private:
-	std::map<std::string, FontFamily > loaded_fonts;
-	std::map<std::string, Resource<Image>> loaded_images;
-};
-*/
 
 Program::Program()
 {
-	/*
-	#ifndef _DEBUG
-	std::string appdata_dir = uicore::Directory::get_appdata("ClanLib", "Carambola", "1.0");
-	CrashReporter crash_reporter(appdata_dir);
-
-	DetectHang detect_hang;
-	#endif
-	*/
-#ifdef WIN32
-	D3DTarget::set_current();
-#else
-	OpenGLTarget::set_current();
-#endif
-
-	//ResourceManager resources;
-	//DisplayCache::set(resources, std::make_shared<DisplayResources>());
-
 	std::string resource_path = "Resources";
+
 #ifdef __APPLE__
 	resource_path = Directory::get_resourcedata("CarambolaChat", resource_path);
 #endif
+
 	ui_thread = UIThread(resource_path);
+
+	UIThread::add_font_face("font-family: 'Source Sans Pro'", "SourceSansPro/SourceSansPro-Regular.ttf");
+	UIThread::add_font_face("font-family: 'Source Sans Pro'; font-weight: bold", "SourceSansPro/SourceSansPro-Bold.ttf");
+	UIThread::add_font_face("font-family: 'Source Sans Pro'; font-style: italic", "SourceSansPro/SourceSansPro-Italic.ttf");
+	UIThread::add_font_face("font-family: 'Source Sans Pro'; font-weight: bold; font-style: italic", "SourceSansPro/SourceSansPro-BoldItalic.ttf");
 
 	chat_window_controller = std::make_shared<ChatWindowViewController>();
 
@@ -125,4 +43,3 @@ bool Program::update()
 {
 	return !exit;
 }
-
